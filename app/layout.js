01b28a8ebcd4301ae8328c8d788b77d458e5605b
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-10 16:24:15 +0100

    added themes, gotta check again

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import Prodivers from '@/app/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <Header />
-        {children}
+        <Prodivers>
+          <Header />
+          {children}
+        </Prodivers>
       </body>
     </html>
   );
--- components/DarkModeSwitch.jsx
+++ components/DarkModeSwitch.jsx
@@ -1,5 +1,22 @@
+'use client';
+
 import React from 'react';
+import { BsSun as LightMode } from 'react-icons/bs';
+import { BsMoon as DarkMode } from 'react-icons/bs';
+import { useTheme } from 'next-themes';
 
 export default function DarkModeSwitch() {
-  return <div>DarkModeSwitch</div>;
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  return (
+    <>
+      {currentTheme === 'dark' ? (
+        <LightMode onClick={() => setTheme('light')} />
+      ) : (
+        <DarkMode onClick={() => setTheme('dark')} />
+      )}
+    </>
+  );
 }

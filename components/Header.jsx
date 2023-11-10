5f907ab4ecfb482f2a6472f1cbba0c958f43c798
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-10 15:56:34 +0100

    adding dark theme/themes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MenuItem from './MenuItem';
+import DarkModeSwitch from './DarkModeSwitch';
 import { AiOutlineHome as HomeIcon } from 'react-icons/ai';
 import { AiOutlineInfoCircle as InfoIcon } from 'react-icons/ai';
 import Link from 'next/link';
@@ -12,7 +13,8 @@ export default function Header() {
 
         <MenuItem title="ABOUT" address="/about" Icon={InfoIcon} />
       </nav>
-      <div>
+      <div className="flex items-center space-x-5">
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl ">
             <span className="font-bold bg-lime-500 py-1 px-2 rounded-lg mr-1">

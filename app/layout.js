edaa14b1f2b8ecf31d842c669ca1a460308afcd9
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-10 15:43:54 +0100

    functionality added to navbar components & icons in sm

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-red-400`}>
+      <body className={`${inter.className}`}>
         <Header />
         {children}
       </body>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import MenuItem from './MenuItem';
+import { AiOutlineHome as HomeIcon } from 'react-icons/ai';
+import { AiOutlineInfoCircle as InfoIcon } from 'react-icons/ai';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <MenuItem title="HOME" address="/" Icon=/>
-        </ul>
+    <header className="flex justify-between max-w-6xl mx-2 sm:mx-auto items-center py-6">
+      <nav className="flex mx-10">
+        <MenuItem title="HOME" address="/" Icon={HomeIcon} />
+
+        <MenuItem title="ABOUT" address="/about" Icon={InfoIcon} />
       </nav>
+      <div>
+        <Link href="/">
+          <h2 className="text-2xl ">
+            <span className="font-bold bg-lime-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
     </header>
   );
 }
